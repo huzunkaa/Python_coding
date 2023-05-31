6280bf8d9d4646302ad670be177d2cf32e475246
huzunkaa <huzunkai@nwafu.edu.cn>
2023-05-31 09:28:00 +0800

    Default Changelist
    Signed-off-by: huzunkaa <huzunkai@nwafu.edu.cn>

--- Shenzhen_data_center/webserver/coding/views.py
+++ Shenzhen_data_center/webserver/coding/views.py
@@ -13,45 +13,14 @@ def homepage(response):
     return render(response,'homepages.html')
 
 
-def line_chart(request):
-    if request.method == 'POST' and request.FILES['txt_file']:
-        txt_file = request.FILES['txt_file']
-        data = []
-        for line in txt_file:
-            data.append(line.decode().strip().split())
+def analysis(response):
+    return render(response, 'analysis.html')
 
-        # 提取数据并生成折线图
-        x_data = []  # x轴数据
-        y_data = []  # y轴数据
-        for row in data:
-            x_data.append(row[0])
-            y_data.append(row[1])
-
-        #使用matplotlib作图
-        fig,ax = plt.subplots()
-        ax.plot(x_data,y_data)
-        ax.set_xlabel('x')
-        ax.set_ylabel('y')
-        ax.set_title('Canvas Plot')
-        canvas = fig.canvas
-
-
-        # 使用pyecharts生成第一个图
-        line1 = Line()
-        line1.add_xaxis(x_data)
-        line1.add_yaxis("折线图", y_data)
-        line1.set_series_opts(label_opts=opts.LabelOpts(is_show=False))
-        line1.set_global_opts(title_opts=opts.TitleOpts(title="折线图"))
-
-        # 将折线图呈现在网页上
-        return render(request, 'chart.html', {'line': canvas})
-    else:
-        return render(request, 'chart.html')
 
 def document(response):
     return render(response,'document.html')
 
-def contect(response):
+def contact(response):
     return render(response,'contect.html')
 
 def database(response):
--- Shenzhen_data_center/webserver/webserver/urls.py
+++ Shenzhen_data_center/webserver/webserver/urls.py
@@ -7,4 +7,9 @@ from django.views import static
 
 urlpatterns = [
     path('', views.homepage),
+    path('document',views.document),
+    path('database',views.database),
+    path('analysis',views.analysis),
+    path('contact',views.contact)
+
 ]
